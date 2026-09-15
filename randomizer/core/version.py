226123c52c40ce6d8f7234b12f9f932e9a8503d6
Heinki <heinki@hotmail.de>
2026-09-15 19:28:33 +0200

    chore: update version

--- randomizer/core/version.py
+++ randomizer/core/version.py
@@ -1,6 +1,6 @@
 """Single source of truth for launcher and APWorld release versions."""
 
-APP_VERSION = '1.37'
+APP_VERSION = '1.38'
 APWORLD_VERSION = '0.6.4'
 ARCHIPELAGO_VERSION = '0.6.7'
 APWORLD_CONTAINER_VERSION = 8
